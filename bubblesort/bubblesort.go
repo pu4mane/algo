f09ddf1e534fb53138aaa11cfef285fd84b88467
pu4mane <pu4mane@gmail.com>
2024-05-31 01:38:48 +0300

    bubblesort done

---
+++ bubblesort/bubblesort.go
@@ -0,0 +1,13 @@
+package bubblesort
+
+func BubbleSort(arr []int) []int {
+
+	for i := 0; i < len(arr)-1; i++ {
+		for j := i + 1; j < len(arr); j++ {
+			if arr[j] < arr[i] {
+				arr[j], arr[i] = arr[i], arr[j]
+			}
+		}
+	}
+	return arr
+}
---
+++ bubblesort/bubblesort_test.go
@@ -0,0 +1,19 @@
+package bubblesort
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestBubbleSort(t *testing.T) {
+	arr := []int{5, 8, 2, 12, 1, 10, 76, 10}
+
+	got := BubbleSort(arr)
+	fmt.Println(got)
+	want := []int{1, 2, 5, 8, 10, 10, 12, 76}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
